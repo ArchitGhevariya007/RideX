f0720fcb6e391616abd163865b817f7dcc57b1ff
Nupul  Kukadiya <nupurkukadiya@gmail.com>
2022-09-19 22:14:48 +0530

    Calling System added

---
+++ Extra Resource/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <style>
+        button{
+            padding:10px 18px;
+            margin:10px;
+        }
+        
+        p{
+            color:blue;
+            font-size:30px;
+        }
+    </style>
+</head>
+<body>
+
+<p>Make A call</p><button><a href="tel:+4733378901"><i class="fa fa-phone"style="font-size:24px"></i></a></button>
+    
+</body>
+</html>
