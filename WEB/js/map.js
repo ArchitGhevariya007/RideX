1807bd222e952239abb946076134e67fdd938d82
Archit Ghevariya <ghevariyaarchit@gmail.com>
2022-09-20 22:37:26 +0530

    Map added

--- WEB/js/map.js
+++ WEB/js/map.js
@@ -66,3 +66,4 @@ var autocomplete1 = new google.maps.places.Autocomplete(input1, options);
 
 var input2 = document.getElementById("to");
 var autocomplete2 = new google.maps.places.Autocomplete(input2, options);
+
